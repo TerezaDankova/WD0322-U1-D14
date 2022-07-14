c0e4dd81a70d93b459970d04bd909bfa500da628
Tereza Dankova <104557274+TerezaDankova@users.noreply.github.com>
2022-07-14 16:41:41 +0200

    hw upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const numberOfCells = 76
+
+function displayCells() {
+    let cellsContainer = document.getElementById('cells-container')
+    for (let cellCount = 1 ; cellCount<=numberOfCells ; cellCount++)
+    {
+        let cellNode = document.createElement('div')
+        let cellText = document.createElement('span')
+        cellNode.appendChild(cellText)
+        cellText.innerText = cellCount
+        cellNode.classList.add('cell')
+        cellsContainer.appendChild(cellNode)
+    }
+}
+displayCells();
+
+
+function randomNumber(){
+    let cellList = document.querySelectorAll('.cell')
+    let randomNumberNode = Math.floor((Math.random() * 76) +1);
+    let buttonValue = document.getElementById('button')
+    buttonValue.value = randomNumberNode
+    cellList[randomNumberNode-1].style.backgroundColor='#436867'
+}
+randomNumber();
